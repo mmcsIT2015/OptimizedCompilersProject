642e357a9e55f9fb5239d13069dfcfeaebb4b664
Valyaev Anton <chekist131@yandex.ru>
2015-09-21 16:00:11 +0300

    Грамматика выражений + ...
    Реализована грамматика выражений, дробные числа, отрицательные числа,
    операции сравнения чисел

---
+++ Teams/Альфа/SimpleLanguage1/ProgramTree.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+
+namespace ProgramTree
+{
+    public enum AssignType { Assign, AssignPlus, AssignMinus, AssignMult, AssignDivide };
+    public enum OperationType { Plus, Minus, Mult, Div};
+    public enum PredicateOperationType { Greater, Less, Equal, Notequal };
+
+    public class Node // базовый класс для всех узлов    
+    {
+    }
+
+    public class ExprNode : Node // базовый класс для всех выражений
+    {
+    }
+
+    public class BinaryNode : ExprNode
+    {
+        public ExprNode LeftOperand { get; set; }
+        public ExprNode RightOperand { get; set; }
+        public OperationType Operation { get; set; }
+        public BinaryNode(ExprNode LeftOperand, ExprNode RightOperand, OperationType Operation)
+        {
+            this.LeftOperand = LeftOperand;
+            this.RightOperand = RightOperand;
+            this.Operation = Operation;
+        }
+    }
+
+    public class IdNode : ExprNode
+    {
+        public string Name { get; set; }
+        public IdNode(string name) { Name = name; }
+    }
+
+    public class IntNumNode : ExprNode
+    {
+        public int Num { get; set; }
+        public IntNumNode(int num) { Num = num; }
+    }
+
+    public class RealNumNode : ExprNode
+    {
+        public double Num { get; set; }
+        public RealNumNode(double num) { Num = num; }
+    }
+
+    public class StatementNode : Node // базовый класс для всех операторов
+    {
+    }
+
+    public class AssignNode : StatementNode
+    {
+        public IdNode Id { get; set; }
+        public ExprNode Expr { get; set; }
+        public AssignType AssOp { get; set; }
+        public AssignNode(IdNode id, ExprNode expr, AssignType assop = AssignType.Assign)
+        {
+            Id = id;
+            Expr = expr;
+            AssOp = assop;
+        }
+    }
+
+    public class CycleNode : StatementNode
+    {
+        public ExprNode Expr { get; set; }
+        public StatementNode Stat { get; set; }
+        public CycleNode(ExprNode expr, StatementNode stat)
+        {
+            Expr = expr;
+            Stat = stat;
+        }
+    }
+
+    public class BlockNode : StatementNode
+    {
+        public List<StatementNode> StList = new List<StatementNode>();
+        public BlockNode(StatementNode stat)
+        {
+            Add(stat);
+        }
+        public void Add(StatementNode stat)
+        {
+            StList.Add(stat);
+        }
+    }
+    public class PredicateBinaryNode : ExprNode
+    {
+        public ExprNode LeftOperand { get; set; }
+        public ExprNode RightOperand { get; set; }
+        public PredicateOperationType Operation { get; set; }
+        public PredicateBinaryNode(ExprNode LeftOperand, ExprNode RightOperand, PredicateOperationType Operation)
+        {
+            this.LeftOperand = LeftOperand;
+            this.RightOperand = RightOperand;
+            this.Operation = Operation;
+        }
+    }
+}
---
+++ Teams/Альфа/SimpleLanguage1/SimpleYacc.y
@@ -0,0 +1,91 @@
+%{
+// Эти объявления добавляются в класс GPPGParser, представляющий собой парсер, генерируемый системой gppg
+    public BlockNode root; // Корневой узел синтаксического дерева 
+    public Parser(AbstractScanner<ValueType, LexLocation> scanner) : base(scanner) { }
+%}
+
+%output = SimpleYacc.cs
+
+%union { 
+			public double dVal; 
+			public int iVal; 
+			public string sVal; 
+			public Node nVal;
+			public ExprNode eVal;
+			public StatementNode stVal;
+			public BlockNode blVal;
+			public PredicateBinaryNode prVal;
+       }
+
+%using ProgramTree;
+
+%namespace SimpleParser
+
+%token BEGIN END CYCLE ASSIGN SEMICOLON PLUS MINUS MULT DIV LEFTBRACKET RIGHTBRACKET GREATER LESS EQUAL NOTEQUAL
+%token <iVal> INUM 
+%token <dVal> RNUM 
+%token <sVal> ID
+
+%type <eVal> expr ident t f
+%type <stVal> assign statement cycle 
+%type <blVal> stlist block
+%type <prVal> prexpr
+
+%%
+
+progr   : block { root = $1; }
+		;
+
+stlist	: statement 
+			{ 
+				$$ = new BlockNode($1); 
+			}
+		| stlist SEMICOLON statement 
+			{ 
+				$1.Add($3); 
+				$$ = $1; 
+			}
+		;
+
+statement: assign { $$ = $1; }
+		| block   { $$ = $1; }
+		| cycle   { $$ = $1; }
+	;
+
+ident 	: ID { $$ = new IdNode($1); }	
+		;
+	
+assign 	: ident ASSIGN expr { $$ = new AssignNode($1 as IdNode, $3); }
+		| ident ASSIGN prexpr { $$ = new AssignNode($1 as IdNode, $3); }
+		;
+
+prexpr	: expr GREATER expr { $$ = new PredicateBinaryNode($1, $3, PredicateOperationType.Greater); }
+		| expr LESS expr { $$ = new PredicateBinaryNode($1, $3, PredicateOperationType.Less); }
+		| expr EQUAL expr { $$ = new PredicateBinaryNode($1, $3, PredicateOperationType.Equal); }
+		| expr NOTEQUAL expr { $$ = new PredicateBinaryNode($1, $3, PredicateOperationType.Notequal); }
+		;
+
+expr	: t { $$ = $1; }
+		| expr PLUS t { $$ = new BinaryNode($1, $3, OperationType.Plus); }
+		| expr MINUS t { $$ = new BinaryNode($1, $3, OperationType.Minus); }
+		;
+
+t		: f { $$ = $1; }
+		| t MULT f { $$ = new BinaryNode($1, $3, OperationType.Mult); }
+		| t DIV f { $$ = new BinaryNode($1, $3, OperationType.Div); }
+		;
+
+f		: ident { $$ = $1 as IdNode; }
+		| INUM { $$ = new IntNumNode($1); }
+		| RNUM { $$ = new RealNumNode($1); }
+		| LEFTBRACKET expr RIGHTBRACKET { $$ = $2; }
+		;
+
+block	: BEGIN stlist END { $$ = $2; }
+		;
+
+cycle	: CYCLE expr statement { $$ = new CycleNode($2, $3); }
+		;
+	
+%%
+
