0a1369a8c0a7bbf73b537f77ed4558c222240fb7
alexey <alexey.a.mikh@gmail.com>
2015-09-26 15:43:46 +0300

    Обновлено readme, добавлен новый интерфейс
    - В readme добавлены комментарии по принципам работы и выполнению первого задания (с оптимизациями).

---
+++ Common/Optimization/IOptimizator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleLang.Optimization
+{
+    interface IOptimizator
+    {
+        public void Optimize();
+    }
+}
