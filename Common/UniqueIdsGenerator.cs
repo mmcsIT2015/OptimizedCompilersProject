f8eb2019941b12b471b0c054fd9a949a3a66adec
alexey <alexey.a.mikh@gmail.com>
2015-10-31 13:05:10 +0300

    TR:1 Облегчен генератор имен

--- Common/UniqueIdsGenerator.cs
+++ Common/UniqueIdsGenerator.cs
@@ -12,28 +12,12 @@ namespace SimpleLang
     {
         private static UniqueIdsGenerator mInstance;                
 
-        //нужно для того, чтобы каждый раз при запуске программы
-        //генерировалась одна и та же последовательность строк
-        //полезно для дебага
-        private const int mSeed = 1;
-
         // Счетчики для генерации удобочитаемых переменных
         private Dictionary<Prefix, int> mCounters = new Dictionary<Prefix, int>();
 
-        //внутренний генератор случайных чисел
-        private Random mRandomGenerator;
-
-        //алфавит символов для генерации строк
-        private string mAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-        private HashSet<string> mUsed = new HashSet<string>();
-
-        /// <summary>
-        /// Да, он приватный. Так надо.
-        /// </summary>
         private UniqueIdsGenerator()
         {
-            mRandomGenerator = new Random(mSeed);
+            
         }
 
         /// <summary>
@@ -45,41 +29,6 @@ namespace SimpleLang
             if (mInstance == null) mInstance = new UniqueIdsGenerator();
 
             return mInstance;
-        }
-
-        /// <summary>
-        /// Возвращает случайную строку
-        /// </summary>
-        /// <returns>Строка</returns>
-        public string Get(int length)
-        {
-            if (length > 64)
-            {
-                // На всякий случай, меру надо знать в длине идентификаторов...
-                throw new ArgumentException("Too long name ID: " + length + ". Maximum length - 64");
-            }
-
-            int attempt = 0;
-            StringBuilder builder;
-            double limit = Math.Pow(mAlphabet.Length, length);
-            do
-            {
-                // @ - Такого символа точно быть в программе не могло
-                builder = new StringBuilder("@");
-                for (int i = 0; i < length; ++i)
-                {
-                    builder.Append(mAlphabet[mRandomGenerator.Next(mAlphabet.Length)]);
-                }
-            }
-            while ((++attempt < limit) && mUsed.Contains(builder.ToString()));
-
-            if (attempt >= limit)
-            {
-                throw new Exception("No free IDs with length " + length);
-            }
-
-            mUsed.Add(builder.ToString());
-            return builder.ToString();
         }
 
         public string Get(String prefix)
