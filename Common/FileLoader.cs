0a74c2ed2d9f81f8e6e448b22554d7cf2d0de22a
jknightmmcs <jediknight.93@mail.ru>
2015-11-13 17:13:01 +0300

    FileLoader changes.
    1. If-else to switch
    2. GetExtension() is used.

--- Common/FileLoader.cs
+++ Common/FileLoader.cs
@@ -15,9 +15,9 @@ namespace SimpleLang
 
         public static GrammarType GetGrammarType(string fileName)
         {
-            string extension = fileName.Substring(fileName.LastIndexOf(".") + 1);
-            if (extension == "pasn") return GrammarType.PASCAL;
-            else if (extension == "cn") return GrammarType.C;
+            string extension = System.IO.Path.GetExtension(fileName);
+            if (extension == ".pasn") return GrammarType.PASCAL;
+            else if (extension == ".cn") return GrammarType.C;
             return GrammarType.UNKNOWN;
         }
 
@@ -30,32 +30,40 @@ namespace SimpleLang
 
         public static BlockNode Parse(string content, GrammarType type)
         {
-            if (type == GrammarType.UNKNOWN)
-                throw new Exception("File extension is unknown");
-            if (type == GrammarType.C)
-            {                
-                SimpleScannerC.Scanner scanner = new SimpleScannerC.Scanner();
-                scanner.SetSource(content, 0);
-                SimpleParserC.Parser parser = new SimpleParserC.Parser(scanner);
+            switch (type)
+            {
+                case GrammarType.UNKNOWN:
+                    throw new Exception("File extension is unknown");
+                case GrammarType.C:
+                    {
+                        SimpleScannerC.Scanner scanner = new SimpleScannerC.Scanner();
+                        scanner.SetSource(content, 0);
+                        SimpleParserC.Parser parser = new SimpleParserC.Parser(scanner);
 
-                //Everything's okay here: exception handling in Main will handle all errors,
-                //so we don't need to check the value;
-                if (parser.Parse())
-                    return parser.root;
-                else
-                    throw new Exception("Parsing error. ");
-            } else {                
-                SimpleScannerPascal.Scanner scanner = new SimpleScannerPascal.Scanner();
-                scanner.SetSource(content, 0);
-                SimpleParserPascal.Parser parser = new SimpleParserPascal.Parser(scanner);
+                        //Everything's okay here: exception handling in Main will handle all errors,
+                        //so we don't need to check the value;
+                        if (parser.Parse())
+                            return parser.root;
+                        else
+                            throw new Exception("Parsing error. ");
+                    }
+                case GrammarType.PASCAL:
+                    {
+                        SimpleScannerPascal.Scanner scanner = new SimpleScannerPascal.Scanner();
+                        scanner.SetSource(content, 0);
+                        SimpleParserPascal.Parser parser = new SimpleParserPascal.Parser(scanner);
 
-                //Everything's okay here: exception handling in Main will handle all errors,
-                //so we don't need to check the value;
-                if (parser.Parse())
-                    return parser.root;
-                else
-                    throw new Exception("Parsing error. ");
-            }
-        }
+                        //Everything's okay here: exception handling in Main will handle all errors,
+                        //so we don't need to check the value;
+                        if (parser.Parse())
+                            return parser.root;
+                        else
+                            throw new Exception("Parsing error. ");
+                    }
+                default:
+                    //Simply cannot happen.
+                    return null;
+            }                        
+        }            
     }
 }
