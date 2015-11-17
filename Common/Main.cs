8a812e1dff696d33846420d6f7eecd7a4ad93419
Alexey <alexey.a.mikh@gmail.com>
2015-11-17 22:38:04 +0300

    TR:1 Добавлена упрощенная система тестирования
    Обходит всю директорию (рекурсивно) и пытается распарсить все файлы с расширениями `.cn`/`.pas`.
    Перед коммитом теперь всегда запускаем тесты и смотрим, чтобы ничего не сломалось.

--- Common/Main.cs
+++ Common/Main.cs
@@ -34,37 +34,39 @@ namespace SimpleCompiler
                 Console.WriteLine();
             }
         }
-        
-        public static void Main()
+
+        public static void RunAllTests()
         {
-            List<string> files = new List<string>();
+            var extensions = new string[] { "*.cn", "*.pasn" };
 
-            files.Add(@"..\..\tests\test-1.cn");
-            //files.Add(@"..\..\tests\test-dce2.cn");
-            //files.Add(@"..\..\in.pasn");
-            //files.Add(@"..\..\a.cn");
-            //files.Add(@"..\..\test_cso.txt"); // Тест для оптимизации: Устранение общих выражений
+            var directory = Directory.GetCurrentDirectory();
+            directory = directory.Substring(0, directory.LastIndexOf('\\'));
+            directory = directory.Substring(0, directory.LastIndexOf('\\'));
 
+            var files = new List<string>();
+            foreach (var ext in extensions)
+            {
+                var temp = Directory.GetFiles(directory, ext, SearchOption.AllDirectories);
+                files.InsertRange(files.Count, temp);
+            }
+
+            ;
             foreach (var file in files)
             {
+                var view = "...\\" + file.Substring(file.LastIndexOf("Common"));
+                Console.Write("Parse file " + view + "... ");
                 try
                 {
-                    var root = FileLoader.LoadFile(file, System.Text.Encoding.UTF8);
-                    
-                    Console.WriteLine("Syntax tree ready");
-
+                    var root = FileLoader.LoadFile(file, System.Text.Encoding.Default);
                     Gen3AddrCodeVisitor codeGenerator = new Gen3AddrCodeVisitor();
                     codeGenerator.Visit(root);
 
                     var code = codeGenerator.CreateCode();
-                    Console.WriteLine(code);
-
-                    //DeadCodeElimination deadCodeElimination = new DeadCodeElimination(code/*, 1*/);
-                    //deadCodeElimination.Optimize();                   
+                    Console.WriteLine("done!");      
                 }
                 catch (FileNotFoundException)
                 {
-                    Console.WriteLine("File not found: "+ file);
+                    Console.WriteLine("File not found: " + file);
                 }
                 catch (LexException e)
                 {
@@ -77,6 +79,60 @@ namespace SimpleCompiler
                 catch (Exception e)
                 {
                     Console.WriteLine("Unexpected error: " + e.Message);
+                }
+            }
+        }
+
+        public static void Main()
+        {
+            bool runTests = false;
+            if (runTests)
+            {
+                RunAllTests();
+            }
+            else
+            {
+                List<string> files = new List<string>();
+
+                files.Add(@"..\..\tests\test-1.cn");
+                //files.Add(@"..\..\tests\test-dce2.cn");
+                //files.Add(@"..\..\in.pasn");
+                //files.Add(@"..\..\a.cn");
+                //files.Add(@"..\..\test_cso.txt"); // Тест для оптимизации: Устранение общих выражений
+
+                foreach (var file in files)
+                {
+                    try
+                    {
+                        var root = FileLoader.LoadFile(file, System.Text.Encoding.UTF8);
+
+                        Console.WriteLine("Syntax tree ready");
+
+                        Gen3AddrCodeVisitor codeGenerator = new Gen3AddrCodeVisitor();
+                        codeGenerator.Visit(root);
+
+                        var code = codeGenerator.CreateCode();
+                        Console.WriteLine(code);
+
+                        //DeadCodeElimination deadCodeElimination = new DeadCodeElimination(code/*, 1*/);
+                        //deadCodeElimination.Optimize();                   
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        Console.WriteLine("File not found: " + file);
+                    }
+                    catch (LexException e)
+                    {
+                        Console.WriteLine("Lexer error: " + e.Message);
+                    }
+                    catch (SyntaxException e)
+                    {
+                        Console.WriteLine("Syntax error: " + e.Message);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Unexpected error: " + e.Message);
+                    }
                 }
             }
 
