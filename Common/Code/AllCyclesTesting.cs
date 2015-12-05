17545010ae5d3b9ecb17bae6902b6de87f7bc685
Valyaev Anton <chekist131@yandex.ru>
2015-12-05 15:46:44 +0300

    Все циклы(тесты): рефакторинг

--- Common/Code/AllCyclesTesting.cs
+++ Common/Code/AllCyclesTesting.cs
@@ -11,12 +11,18 @@ namespace iCompiler
     public static class AllCyclesTesting
     {
         /// <summary>
-        /// Абстрактый тестовый граф
+        /// Тестовый граф
         /// </summary>
-        public abstract class AbstarctTestGraph : IGraph<int>
+        public class TestGraph : IGraph<int>
         {
-            protected Dictionary<int, IList<int>> Data;
-            protected Dictionary<int, IList<int>> ReversedData;
+            public TestGraph(Dictionary<int, List<int>> data)
+            {
+                this.Data = data;
+                ReversedData = TestGraph.getReversedIndexedGraph(Data);
+            }
+
+            private Dictionary<int, List<int>> Data;
+            private Dictionary<int, List<int>> ReversedData;
             public IEnumerable<int> OutEdges(int block)
             {
                 return Data[block];
@@ -27,7 +33,7 @@ namespace iCompiler
                 return ReversedData[block];
             }
 
-            protected static Dictionary<int, IList<int>> getReversedIndexedGraph(Dictionary<int, IList<int>> graph)
+            private static Dictionary<int, List<int>> getReversedIndexedGraph(Dictionary<int, List<int>> graph)
             {
                 bool[,] graphTable = new bool[graph.Count() + 1, graph.Count() + 1];
                 graphTable.Initialize();
@@ -39,7 +45,7 @@ namespace iCompiler
                     }
                 }
 
-                Dictionary<int, IList<int>> reversedGraph = new Dictionary<int, IList<int>>();
+                Dictionary<int, List<int>> reversedGraph = new Dictionary<int, List<int>>();
                 for (int i = 1; i <= graph.Count(); i++) reversedGraph[i] = new List<int>(2);
                 for (int i = 1; i <= graph.Count(); i++)
                 {
@@ -53,16 +59,19 @@ namespace iCompiler
             }
         }
         /// <summary>
-        /// Абстрактное тестовое дерево доминаторов
+        /// Тестовое дерево доминаторов
         /// </summary>
-        public abstract class AbstractTestDominatorTree : IDominatorRelation<int>
+        public class TestDominatorTree : IDominatorRelation<int>
         {
-            protected Dictionary<int, List<int>> data;
+            public TestDominatorTree(Dictionary<int, List<int>> data)
+            {
+                this.data = data;
+            }
+            private Dictionary<int, List<int>> data;
             public bool FirstDomSeccond(int a, int b)
             {
                 return data[b].Any(x => x == a);
             }
-
             public IEnumerable<int> UpperDominators(int a)
             {
                 return data[a];
@@ -74,19 +83,19 @@ namespace iCompiler
         public abstract class AllCyclesTestExampleAbstractFactory
         {
             protected string exampleTitle;
-            public abstract int[] GetBlocks();
-            public abstract AbstarctTestGraph GetGraph();
-            public abstract List<DomGraph.BlocksPair<int>> GetReverseArcs();
-            public abstract AbstractTestDominatorTree GetDomTree();
+            protected int[] blocks;
+            protected TestGraph graph;
+            protected List<DomGraph.BlocksPair<int>> reverseEdges;
+            protected TestDominatorTree domTree;
             public void TestAllCycles()
             {
-                AllCycles<int> allCycles = new AllCycles<int>(GetBlocks(), GetGraph(), GetReverseArcs(), GetDomTree());
+                AllCycles<int> allCycles = new AllCycles<int>(blocks, graph, reverseEdges, domTree);
                 Console.WriteLine("AllCycles");
                 _cycles(allCycles);
             }
             public void TestAllCyclesWithSpecialCase()
             {
-                AllCycles<int> allCycles = new AllCyclesWithSpecialCase<int>(GetBlocks(), GetGraph(), GetReverseArcs(), GetDomTree());
+                AllCycles<int> allCycles = new AllCyclesWithSpecialCase<int>(blocks, graph, reverseEdges, domTree);
                 Console.WriteLine("AllCyclesWithSpecialCase");
                 _cycles(allCycles);
             }
@@ -115,10 +124,12 @@ namespace iCompiler
                 }
                 Console.WriteLine();
             }
-            private void _spanningTree(SpanningTree<int> t)
+            public void TestSpanningTree()
             {
+                SpanningTree<int> t = new SpanningTreeWithoutRecursive<int>(blocks, graph);
+                Console.WriteLine("SpanningTree");
                 Console.WriteLine(exampleTitle);
-                for (int i = GetBlocks().First(); i <= GetBlocks().Last(); i++)
+                for (int i = blocks.First(); i <= blocks.Last(); i++)
                 {
                     Console.Write(i + "(" + t.Numbers[i] + "): ");
                     foreach (int v in t.Data[i])
@@ -127,151 +138,87 @@ namespace iCompiler
                 }
                 Console.WriteLine();
             }
-            public void TestSpanningTree()
-            {
-                SpanningTree<int> t = new SpanningTreeWithoutRecursive<int>(GetBlocks(), GetGraph());
-                Console.WriteLine("SpanningTree");
-                _spanningTree(t);
-            }
         }
         /// <summary>
         /// Пример 1 из лекции
         /// </summary>
-        public class AllCyclesTestExample : AllCyclesTestExampleAbstractFactory
+        public class AllCyclesTestExample1 : AllCyclesTestExampleAbstractFactory
         {
-            public AllCyclesTestExample() { exampleTitle = "Example1"; }
-
-            private class TestGraph : AbstarctTestGraph
-            {
-                public TestGraph()
-                {
-                    Data = new Dictionary<int, IList<int>>();
-                    for (int i = 1; i <= 10; i++)
-                        Data[i] = new List<int>();
-                    Data[1].Add(2);
-                    Data[1].Add(3);
-                    Data[2].Add(3);
-                    Data[3].Add(4);
-                    Data[4].Add(5);
-                    Data[4].Add(6);
-                    Data[4].Add(3);
-                    Data[5].Add(7);
-                    Data[6].Add(7);
-                    Data[7].Add(8);
-                    Data[7].Add(4);
-                    Data[8].Add(9);
-                    Data[8].Add(10);
-                    Data[8].Add(3);
-                    Data[9].Add(1);
-                    Data[10].Add(7);
-                    ReversedData = getReversedIndexedGraph(Data);
-                }
-            }
-
-            public override int[] GetBlocks()
-            {
-                return new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            }
-
-            public override AbstarctTestGraph GetGraph()
-            {
-                return new TestGraph();
-            }
-
-            public override List<DomGraph.BlocksPair<int>> GetReverseArcs()
-            {
-                List<DomGraph.BlocksPair<int>> reverseEdges = new List<DomGraph.BlocksPair<int>>();
+            public AllCyclesTestExample1() 
+            { 
+                exampleTitle = "Example1";
+                blocks = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                Dictionary<int, List<int>>  Data = new Dictionary<int, List<int>>();
+                for (int i = 1; i <= 10; i++)
+                    Data[i] = new List<int>();
+                Data[1].Add(2);
+                Data[1].Add(3);
+                Data[2].Add(3);
+                Data[3].Add(4);
+                Data[4].Add(5);
+                Data[4].Add(6);
+                Data[4].Add(3);
+                Data[5].Add(7);
+                Data[6].Add(7);
+                Data[7].Add(8);
+                Data[7].Add(4);
+                Data[8].Add(9);
+                Data[8].Add(10);
+                Data[8].Add(3);
+                Data[9].Add(1);
+                Data[10].Add(7);
+                graph = new TestGraph(Data);
+                reverseEdges = new List<DomGraph.BlocksPair<int>>();
                 reverseEdges.Add(new DomGraph.BlocksPair<int>(9, 1));
                 reverseEdges.Add(new DomGraph.BlocksPair<int>(4, 3));
                 reverseEdges.Add(new DomGraph.BlocksPair<int>(8, 3));
                 reverseEdges.Add(new DomGraph.BlocksPair<int>(7, 4));
                 reverseEdges.Add(new DomGraph.BlocksPair<int>(10, 7));
-                return reverseEdges;
-            }
-
-            public override AbstractTestDominatorTree GetDomTree()
-            {
-                return new TestDominatorTree();
-            }
-
-            private class TestDominatorTree : AbstractTestDominatorTree
-            {
-                public TestDominatorTree()
-                {
-                    data = new Dictionary<int, List<int>>();
-                    for (int i = 1; i <= 10; i++)
-                        data[i] = new List<int>();
-                    data[1].AddRange(new int[] { 1 });
-                    data[2].AddRange(new int[] { 1, 2 });
-                    data[3].AddRange(new int[] { 1, 3 });
-                    data[4].AddRange(new int[] { 1, 3, 4 });
-                    data[5].AddRange(new int[] { 1, 3, 4, 5 });
-                    data[6].AddRange(new int[] { 1, 3, 4, 6 });
-                    data[7].AddRange(new int[] { 1, 3, 4, 7 });
-                    data[8].AddRange(new int[] { 1, 3, 4, 7, 8 });
-                    data[9].AddRange(new int[] { 1, 3, 4, 7, 8, 9 });
-                    data[10].AddRange(new int[] { 1, 3, 4, 7, 8, 10 });
-                }
+                Dictionary<int, List<int>> DataDom = new Dictionary<int, List<int>>();
+                for (int i = 1; i <= 10; i++)
+                    DataDom[i] = new List<int>();
+                DataDom[1].AddRange(new int[] { 1 });
+                DataDom[2].AddRange(new int[] { 1, 2 });
+                DataDom[3].AddRange(new int[] { 1, 3 });
+                DataDom[4].AddRange(new int[] { 1, 3, 4 });
+                DataDom[5].AddRange(new int[] { 1, 3, 4, 5 });
+                DataDom[6].AddRange(new int[] { 1, 3, 4, 6 });
+                DataDom[7].AddRange(new int[] { 1, 3, 4, 7 });
+                DataDom[8].AddRange(new int[] { 1, 3, 4, 7, 8 });
+                DataDom[9].AddRange(new int[] { 1, 3, 4, 7, 8, 9 });
+                DataDom[10].AddRange(new int[] { 1, 3, 4, 7, 8, 10 });
+                domTree = new TestDominatorTree(DataDom);
             }
         }
         /// <summary>
         /// Пример 2 из лекции
         /// </summary>
-        public class AllCyclesTestExample1 : AllCyclesTestExampleAbstractFactory
+        public class AllCyclesTestExample2 : AllCyclesTestExampleAbstractFactory
         {
-            public AllCyclesTestExample1() { exampleTitle = "Example2"; }
-
-            private class TestDominatorTree1 : AbstractTestDominatorTree
-            {
-                public TestDominatorTree1()
-                {
-                    data = new Dictionary<int, List<int>>();
-                    for (int i = 1; i <= 4; i++)
-                        data[i] = new List<int>();
-                    data[1].AddRange(new int[] { 1 });
-                    data[2].AddRange(new int[] { 1, 2 });
-                    data[3].AddRange(new int[] { 1, 2, 3 });
-                    data[4].AddRange(new int[] { 1, 2, 4 });
-                }
-            }
-
-            private class TestGraph1 : AbstarctTestGraph
-            {
-                public TestGraph1()
-                {
-                    Data = new Dictionary<int, IList<int>>();
-                    for (int i = 1; i <= 4; i++)
-                        Data[i] = new List<int>();
-                    Data[1].Add(2);
-                    Data[2].Add(3);
-                    Data[2].Add(4);
-                    Data[3].Add(1);
-                    Data[4].Add(1);
-                    ReversedData = getReversedIndexedGraph(Data);
-                }
-            }
-
-            public override int[] GetBlocks()
-            {
-                return new int[] { 1, 2, 3, 4 };
-            }
-
-            public override AbstarctTestGraph GetGraph()
-            {
-                return new TestGraph1();
-            }
-
-            public override List<DomGraph.BlocksPair<int>> GetReverseArcs()
-            {
-                List<DomGraph.BlocksPair<int>> reverseEdges1 = new List<DomGraph.BlocksPair<int>>();
-                reverseEdges1.Add(new DomGraph.BlocksPair<int>(3, 1));
-                reverseEdges1.Add(new DomGraph.BlocksPair<int>(4, 1));
-                return reverseEdges1;
-            }
-
-            public override AbstractTestDominatorTree GetDomTree()
-            {
-                return new TestDominatorTree1();
+            public AllCyclesTestExample2() 
+            { 
+                exampleTitle = "Example2";
+                blocks = new int[] { 1, 2, 3, 4 };
+                Dictionary<int, List<int>> Data = new Dictionary<int, List<int>>();
+                for (int i = 1; i <= 4; i++)
+                    Data[i] = new List<int>();
+                Data[1].Add(2);
+                Data[2].Add(3);
+                Data[2].Add(4);
+                Data[3].Add(1);
+                Data[4].Add(1);
+                graph = new TestGraph(Data);
+                reverseEdges = new List<DomGraph.BlocksPair<int>>();
+                reverseEdges.Add(new DomGraph.BlocksPair<int>(3, 1));
+                reverseEdges.Add(new DomGraph.BlocksPair<int>(4, 1));
+                Dictionary<int, List<int>> DataDom = new Dictionary<int, List<int>>();
+                for (int i = 1; i <= 4; i++)
+                    DataDom[i] = new List<int>();
+                DataDom[1].AddRange(new int[] { 1 });
+                DataDom[2].AddRange(new int[] { 1, 2 });
+                DataDom[3].AddRange(new int[] { 1, 2, 3 });
+                DataDom[4].AddRange(new int[] { 1, 2, 4 });
+                domTree = new TestDominatorTree(DataDom);
             }
         }
         /// <summary>
@@ -279,22 +226,22 @@ namespace iCompiler
         /// </summary>
         public static void TestAllCycles()
         {
-            AllCyclesTestExample ex = new AllCyclesTestExample();
             AllCyclesTestExample1 ex1 = new AllCyclesTestExample1();
-            ex.TestAllCycles();
+            AllCyclesTestExample2 ex2 = new AllCyclesTestExample2();
             ex1.TestAllCycles();
-            ex.TestAllCyclesWithSpecialCase();
+            ex2.TestAllCycles();
             ex1.TestAllCyclesWithSpecialCase();
+            ex2.TestAllCyclesWithSpecialCase();
         }
         /// <summary>
         /// Тесты для классов SpanningTree
         /// </summary>
         public static void TestSpanningTree()
         {
-            AllCyclesTestExample ex = new AllCyclesTestExample();
-            ex.TestSpanningTree();
             AllCyclesTestExample1 ex1 = new AllCyclesTestExample1();
+            AllCyclesTestExample2 ex2 = new AllCyclesTestExample2();
             ex1.TestSpanningTree();
+            ex2.TestSpanningTree();
         }
     }
 }
