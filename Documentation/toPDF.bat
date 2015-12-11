6c1b79256ecb119260a3a41aee2b5c8cda8b6cb6
Valyaev Anton <chekist131@yandex.ru>
2015-12-11 23:23:48 +0300

    Обновлен скрипт
    можно запускать из любой папки, компилируется в docx, не требует модуля
    MikTeX, OptimizedCompilersProjectDocumentation.txt удаляется как
    временный

--- Documentation/toPDF.bat
+++
@@ -1,4 +0,0 @@
-DocumentationAssembler
-cd Pictures
-"C:\Program Files (x86)\Pandoc\pandoc" --latex-engine=xelatex -V mainfont="Sitka Text" --highlight-style tango ..\OptimizedCompilersProjectDocumentation.txt -o ..\OptimizedCompilersProjectDocumentation.pdf
-cd ..
