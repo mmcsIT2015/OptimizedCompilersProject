2207891864126c619b475c6748abecff9aa86abb
Valyaev Anton <chekist131@yandex.ru>
2015-12-12 14:57:51 +0300

    Скрипт запускать только из папки Documentation

--- Documentation/toDOCX.bat
+++ Documentation/toDOCX.bat
@@ -1,6 +1,6 @@
 DocumentationAssembler
-cd %~dp0
 cd Pictures
 "C:\Program Files (x86)\Pandoc\pandoc" --latex-engine=xelatex -V mainfont="Sitka Text" --highlight-style tango ..\OptimizedCompilersProjectDocumentation.txt -o ..\OptimizedCompilersProjectDocumentation.docx
 cd ..
 del OptimizedCompilersProjectDocumentation.txt
+pause
