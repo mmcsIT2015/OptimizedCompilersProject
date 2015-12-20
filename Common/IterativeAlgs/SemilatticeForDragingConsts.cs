2a5014903f73be844b0d56b739774f1fde594c86
BatMich <m1h-bat@yandex.ru>
2015-12-20 05:38:45 +0300

    Добавлена полурешетка для алгоритма протяжки констант.

---
+++ Common/IterativeAlgs/SemilatticeForDragingConsts.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iCompiler.Line;
+using ProgramTree;
+namespace iCompiler
+{
+
+
+    class SemilatticeForDragingConsts : ISemilattice<ConstNACInfo>
+    {
+
+        private List<ConstNACInfo> mTop = new List<ConstNACInfo>();
+
+        public SemilatticeForDragingConsts()
+        {
+
+        }
+
+        
+        public IEnumerable<ConstNACInfo> Join(IEnumerable<ConstNACInfo> lhs, IEnumerable<ConstNACInfo> rhs)
+        {
+            List<ConstNACInfo> resL = new List<ConstNACInfo>();
+            var lrhs = lhs.Union(rhs);
+            foreach(var e in lrhs)
+            {
+                var tmpr = rhs.First(e.NameEquals);
+                var tmpl = rhs.First(e.NameEquals);
+                resL.Add(ConstNACInfo.MaxLowerBound(tmpl, tmpr));
+            }
+            return resL;
+        }
+
+        public IEnumerable<ConstNACInfo> Top()
+        {
+            return mTop;
+        }
+    }
+}
